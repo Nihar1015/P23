a44698eacb15f1096c02ab995437d117a6925cf0
Nihar1015 <69799996+Nihar1015@users.noreply.github.com>
2020-11-12 15:53:55 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -43,10 +43,8 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:3, isStatic:true});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.6, isStatic:true});
 	World.add(world, packageBody);
-	Matter.Body.setStatic(packageBody,false);
-	packageBody.restitution = 0.6;
 	
 
 	//Create a Ground
